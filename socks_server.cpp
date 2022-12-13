7a1a16bab32252d83e3c803b4f8a76d8b13c0973
Shung <jonson111088@gmail.com>
2022-12-13 13:50:15 +0000

    Complete BIND

--- socks_server.cpp
+++ socks_server.cpp
@@ -19,15 +19,17 @@ using namespace std;
 boost::asio::io_service my_io_service;
 boost::asio::io_context io_context;
 
-typedef struct socks_header {
+typedef struct socks_info {
     uint8_t  version;
     uint8_t  command;
     uint16_t port;
+    uint16_t bind_port;
     string   address;
     string   hostname;
-} SocksHeader;
+    bool     need_resolve;
+    bool     is_accept;
+} SocksInfo;
 
-const uint8_t SOCKS_VERSION_4 = 04;
 const uint8_t COMMAND_CONNECT = 1;
 const uint8_t COMMAND_BIND    = 2;
 const uint8_t COMMAND_ACCEPT  = 90;
@@ -87,19 +89,36 @@ inline void show_error(const char *func_name, int code, string msg) {
 
 class ProxyHandler: public enable_shared_from_this<ProxyHandler> {
 public:
-    ProxyHandler(tcp::socket sock): inner_sock(move(sock)), outer_sock(io_context), resolver_(io_context) {
+    ProxyHandler(tcp::socket sock): inner_sock(move(sock)), outer_sock(io_context), resolver_(io_context), acceptor_(io_context) {
         bzero(inner_rx_buffer, BUFFER_SIZE);
         bzero(inner_tx_buffer, BUFFER_SIZE);
         bzero(outer_rx_buffer, BUFFER_SIZE);
         bzero(outer_tx_buffer, BUFFER_SIZE);
+
+        parse_firewall();
     }
 
     void start() {
-        do_inner_read_control();
+        check_firewall();
+        if (request.is_accept == true) {
+            do_inner_read_control();
+        } else {
+            // TODO
+        }
+    }
+
+    void parse_firewall() {
+        // TODO
+    }
+
+    void check_firewall() {
+        // TODO
+
+        request.is_accept = true;
     }
 
     void do_inner_read_control() {
-        #if 1
+        #if 0
         cout << "do_inner_read_control" << endl;
         #endif
         /* Handle SOCKS Request */
@@ -107,13 +126,14 @@ public:
         inner_sock.async_read_some(boost::asio::buffer(inner_rx_buffer, BUFFER_SIZE),
             [this, self](boost::system::error_code ec, size_t length) {
                 if (!ec) {
-                    bool need_resolve = false;
-
                     // Get SOCKS Request
                     request.version = inner_rx_buffer[0];
                     request.command = inner_rx_buffer[1];
                     request.port = raw_port(inner_rx_buffer[2], inner_rx_buffer[3]);
                     request.address = raw_ip2str(inner_rx_buffer[4], inner_rx_buffer[5], inner_rx_buffer[6], inner_rx_buffer[7]);
+                    request.need_resolve = false;
+
+                    // Check it it has hostname
                     if (request.address.find("0.0.0.") != string::npos) {
                         char tmp_buffer[256] = {'\0'};
 
@@ -125,17 +145,23 @@ public:
                         }
 
                         request.hostname = string(tmp_buffer);
-                        need_resolve = true;
-                        #if 1
+                        request.need_resolve = true;
+                        #if 0
                         cout << "Request.Hostname: " << request.hostname << endl;
                         #endif
                     }
 
-                    // Establish the connection to foreign server
-                    if(need_resolve) {
-                        do_resolve();
-                    } else {
-                        do_connect();
+                    // Handle by command
+                    switch (request.command) {
+                    case COMMAND_CONNECT:
+                        handle_connect();
+                        break;
+                    case COMMAND_BIND:
+                        handle_bind();
+                        break;
+                    default:
+                        cerr << "Unknown command: " << request.command << endl;
+                        break;
                     }
                 } else {
                     show_error("do_inner_read", ec.value(), ec.message());
@@ -145,8 +171,44 @@ public:
         );
     }
 
+    void handle_connect() {
+        // Establish the connection to foreign server
+        if(request.need_resolve) {
+            do_resolve();
+        } else {
+            do_connect();
+        }
+    }
+
+    void handle_bind() {
+        // Bind a random port on SOCKS Server
+        auto self(shared_from_this());
+        uint16_t port;
+
+        while (true) {
+            boost::system::error_code ec;
+            port = rand() % 10000 + 30000;
+            tcp::endpoint ep(tcp::v4(), port);
+            acceptor_.open(ep.protocol());
+
+            acceptor_.bind(ep, ec);
+            if (!ec) {
+                break;
+            } else {
+                acceptor_.close();
+            }
+        }
+        acceptor_.listen();
+        request.bind_port = port;
+        do_write_reply();
+
+        #if 0
+        cout << "Bind Port: " << request.bind_port << endl;
+        #endif
+    }
+
     void do_resolve() {
-        #if 1
+        #if 0
         cout << "do_resolve" << endl;
         #endif
         tcp::resolver::query q(request.hostname, to_string(request.port));
@@ -163,11 +225,11 @@ public:
     }
 
     void do_connect() {
-        #if 1
+        #if 0
         cout << "do_connect" << endl;
         #endif
         tcp::endpoint remote_ep(address::from_string(request.address), request.port);
-        #if 1
+        #if 0
         cout << "Connect to: " << remote_ep.address().to_string() << ":" << remote_ep.port() << endl;
         #endif
 
@@ -175,10 +237,12 @@ public:
         outer_sock.async_connect(remote_ep, 
             [this, self](boost::system::error_code ec) {
                 if (!ec) {
-                    do_write_reply(true);
+                    request.is_accept = true;
+                    do_write_reply();
                 } else {
                     show_error("do_connect", ec.value(), ec.message());
-                    do_write_reply(false);
+                    request.is_accept = false;
+                    do_write_reply();
                     do_close();
                 }
             }
@@ -186,25 +250,27 @@ public:
     }
 
     void do_connect(tcp::resolver::iterator iter) {
-        #if 1
+        #if 0
         cout << "do_connect with iter" << endl;
         #endif
         auto self(shared_from_this());
         outer_sock.async_connect(*iter,
             [this, self, iter](boost::system::error_code ec) {
                 if (!ec) {
-                    do_write_reply(true);
+                    request.is_accept = true;
+                    do_write_reply();
                 } else {
                     show_error("do_connect with iter", ec.value(), ec.message());
-                    do_write_reply(false);
+                    request.is_accept = false;
+                    do_write_reply();
                     do_close();
                 }
             }
         );
     }
 
-    void do_write_reply(bool is_sucessful) {
-        #if 1
+    void do_write_reply() {
+        #if 0
         cout << "do_write_reply" << endl;
         #endif
         char reply[SOCKS_REPLY_SIZE] = {'\0'};
@@ -212,21 +278,35 @@ public:
         // Version
         reply[0] = 0;
         // Command
-        reply[1] = ((is_sucessful == true) ? COMMAND_ACCEPT : COMMAND_REJECT);
+        reply[1] = ((request.is_accept == true) ? COMMAND_ACCEPT : COMMAND_REJECT);
         // Address and Port
         if (request.command == COMMAND_CONNECT) {
-            // Ignore address and port
+            // CONNECT, ignore address and port
         } else {
-            // TODO
+            // BIND, setup port and use wildcard address
+            reply[2] = request.bind_port / 256;
+            reply[3] = request.bind_port % 256;
         }
 
         auto self(shared_from_this());
         inner_sock.async_write_some(boost::asio::buffer(reply, SOCKS_REPLY_SIZE),
             [this, self](boost::system::error_code ec, size_t length) {
                 if (!ec) {
-                    // Start data session
-                    do_inner_data_read();
-                    do_outer_data_read();
+                    if (request.is_accept) {
+                        /* Accept */
+                        if (request.command == COMMAND_CONNECT) {
+                            /* CONNECT */
+                            start_data_session();
+                        } else {
+                            /* BIND */
+                            do_accept();
+                        }
+                    } else {
+                        /* Reject */
+                        do_close();
+                    }
+
+                    show_socks();
                 } else {
                     show_error("do_write_reply", ec.value(), ec.message());
                     do_close();
@@ -235,15 +315,39 @@ public:
         );
     }
 
+    void do_accept() {
+        // For bind operation
+        auto self(shared_from_this());
+        acceptor_.async_accept(
+            [this, self](boost::system::error_code ec, tcp::socket socket) {
+                if (!ec) {
+                    outer_sock = move(socket);
+                } else {
+                    show_error("ProxyHandler do_accept", ec.value(), ec.message());
+                }
+
+                // Only accept one connection
+                acceptor_.close();
+                // Start data session
+                start_data_session();
+            }
+        );
+    }
+
+    void start_data_session() {
+        do_inner_data_read();
+        do_outer_data_read();
+    }
+
     void do_inner_data_read() {
-        #if 1
+        #if 0
         cout << "do_inner_data_read waiting..." << endl;
         #endif
         auto self(shared_from_this());
         inner_sock.async_read_some(boost::asio::buffer(inner_rx_buffer, BUFFER_SIZE),
             [this, self](boost::system::error_code ec, size_t length) {
                 if (!ec) {
-                    #if 1
+                    #if 0
                     cout << "do_inner_data_read got " << length << " data" << endl;
                     #endif
                     // Copy data to outgoing buffer
@@ -256,6 +360,8 @@ public:
                 } else {
                     if (ec.value() == boost::asio::error::eof) {
                         do_close();
+                    } else if (ec.value() == 125) {
+                        // Ignore Operation canceled
                     } else {
                         show_error("do_inner_data_read", ec.value(), ec.message());
                         do_close();
@@ -266,14 +372,14 @@ public:
     }
     
     void do_inner_data_write(size_t data_length) {
-        #if 1
+        #if 0
         cout << "do_inner_data_write" << endl;
         #endif
         auto self(shared_from_this());
         inner_sock.async_write_some(boost::asio::buffer(inner_tx_buffer, data_length),
             [this, self](boost::system::error_code ec, size_t length) {
                 if (!ec) {
-                    #if 1
+                    #if 0
                     cout << "do_inner_data_write write " << length << " data" << endl;
                     #endif
                     // Clean Buffer
@@ -287,14 +393,14 @@ public:
     }
 
     void do_outer_data_read() {
-        #if 1
+        #if 0
         cout << "do_outer_data_read waiting..." << endl;
         #endif
         auto self(shared_from_this());
         outer_sock.async_read_some(boost::asio::buffer(outer_rx_buffer, BUFFER_SIZE),
             [this, self](boost::system::error_code ec, size_t length) {
                 if (!ec) {
-                    #if 1
+                    #if 0
                     cout << "do_outer_data_read got " << length << " data" << endl;
                     #endif
                     // Copy data to inner tx buffer
@@ -317,14 +423,14 @@ public:
     }
 
     void do_outer_data_write(size_t data_length) {
-        #if 1
+        #if 0
         cout << "do_outer_data_write" << endl;
         #endif
         auto self(shared_from_this());
         outer_sock.async_write_some(boost::asio::buffer(outer_tx_buffer, data_length),
             [this, self](boost::system::error_code ec, size_t length) {
                 if (!ec) {
-                    #if 1
+                    #if 0
                     cout << "do_outer_data_write write " << length << " data" << endl;
                     #endif
                     // Clean Buffer
@@ -338,7 +444,7 @@ public:
     }
     
     void do_close() {
-        #if 1
+        #if 0
         cout << "do_close" << endl;
         #endif
         inner_sock.close();
@@ -354,13 +460,14 @@ public:
             << "<D_IP>: " << request.address << endl
             << "<D_PORT>: " << request.port << endl
             << "<Command>: " << cmd2str(request.command) << endl
-            << "<Reply>: " << cmd2str(90) << endl;
+            << "<Reply>: " << cmd2str((request.is_accept == true) ? COMMAND_ACCEPT : COMMAND_REJECT) << endl;
     }
 
 private:
     tcp::socket inner_sock, outer_sock;
-    SocksHeader request;
+    SocksInfo request;
     tcp::resolver resolver_;
+    tcp::acceptor acceptor_;
     
     char inner_rx_buffer[BUFFER_SIZE];
     char inner_tx_buffer[BUFFER_SIZE];
